410ad7eac27cb823da609b99e88b1be94a2a0140
Vivek Patel <vivek_patel@umail.ucsb.edu>
2016-02-29 20:17:03 -0800

    Finished sudoku

--- sudoku.pl
+++ sudoku.pl
@@ -62,7 +62,7 @@ drop([_|Tail], Num, Rest) :-
 % for `getCube`.
 % take: (InputList, NumToTake, ResultList)
 take([], _, []).
-take(_, 0, []).
+take([H|T], 0, []).
 take([Head|Tail], Num, [Head|Rest]) :-
         Num > 0,
         NewNum is Num - 1,
@@ -84,7 +84,7 @@ sublist(List, Start, End, NewList) :-
 % 3  4  5
 % 6  7  8
 %
-% getCube: (Board, ColumnIndex, ContentsOfCube)
+% getCube: (Board, CubeIndex, ContentsOfCube)
 getCube(Board, Number, AsList) :-
         cubeBounds(RowLow, RowHigh, ColLow, ColHigh, Number),
         sublist(Board, RowLow, RowHigh, [Row1, Row2, Row3]),
@@ -92,16 +92,69 @@ getCube(Board, Number, AsList) :-
         sublist(Row2, ColLow, ColHigh, Row2Nums),
         sublist(Row3, ColLow, ColHigh, Row3Nums),
         append(Row1Nums, Row2Nums, TempRow),
-        append(TempRow, Row3Nums, AsList)                
+        append(TempRow, Row3Nums, AsList).                
+
+
+holder(Board, cols([Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9]), cubes([Cube1, Cube2, Cube3, Cube4, Cube5, Cube6, Cube7, Cube8, Cube9])) :-
+    columnAsList(Board, 0, Col1), columnAsList(Board, 1, Col2), columnAsList(Board, 2, Col3), columnAsList(Board, 3, Col4), columnAsList(Board, 4, Col5), columnAsList(Board, 5, Col6), columnAsList(Board, 6, Col7), columnAsList(Board, 7, Col8), columnAsList(Board, 8, Col9),
+    getCube(Board, 0, Cube1), getCube(Board, 1, Cube2), getCube(Board, 2, Cube3), getCube(Board, 3, Cube4), getCube(Board, 4, Cube5), getCube(Board, 5, Cube6),getCube(Board, 6, Cube7), getCube(Board, 7, Cube8), getCube(Board, 8, Cube9).
+
 
 % Given a board, solve it in-place.
 % After calling `solve` on a board, the board should be fully
 % instantiated with a satisfying Sudoku solution.
+
 solve(Board) :-
-	solve(Board, )
+	write('Solving board...\n'),
+	holder(Board, cols(Cols), cubes(Cubes)),
+	solveRow(Board,Board,0,Cols,Cubes).
 
-solve([[]]).
+solveRow(Board,[H|T],Row, Cols,Cubes) :-
+	%% write(Row),
+	%% write('\n'),
+	%% write('Checking row constraint...\n'),
+	Row < 9,
+	%% write('Solving single row...\n'),
+	solveCol(Board,H,H,Row,0,Cols,Cubes),
+	N is Row + 1,
+	solveRow(Board,T,N,Cols,Cubes).
 
+%%solveRow(Board,[],Row,_,_).
+
+solveRow(_,_,9,_,_).
+
+solveCol(Board,FullRow,[H|T],Row,Col,Cols,Cubes) :-
+	Col < 9,
+	%% write(Col),
+	%% write('\n'),
+	%% write('Solving singleton item...\n'),
+	solveItem(Board,FullRow,H,Row,Col,Cols,Cubes),
+	N is Col + 1,
+	solveCol(Board,FullRow,T,Row,N,Cols,Cubes).
+
+%%solveCol(Board,FullRow,[],Row,Col,_,_).	
+
+solveCol(_,_,_,_,9,_,_).
+
+solveItem(Board,FullRow,H,Row,Col,Cols,Cubes) :-
+	nonvar(H);
+	%% write('Row: '),
+	%% write(Row),
+	%% write(', Col: '),
+	%% write(Col),
+	%% write(', '), 
+	(var(H),
+	digit(H),
+	cubeNum(Row,Col,CubeNum),
+	%% write(H), nl,
+	%% getCube(Board, CubeN, L1),
+	%% columnAsList(Board, Col, L2),
+	nth0(Col, Cols, NewColumn),
+	nth0(CubeNum, Cubes, NewCube),
+	is_set(NewColumn),
+	is_set(NewCube),
+	is_set(FullRow)).
+			
 
 % Prints out the given board.
 printBoard([]).
@@ -134,3 +187,17 @@ test2(Board) :-
                  [_, _, 3, _, 6, 2, _, _, _]],
         solve(Board),
         printBoard(Board).
+
+
+test3(Board) :-
+	Board = [[_, _, _, 7, 9, 1, 8, _, _],
+                 [_, _, _, _, _, 4, 3, 1, 7],
+                 [_, _, 6, 3, _, _, _, 2, 9],
+                 [7, _, _, _, 2, _, _, _, _],
+                 [5, 1, _, _, _, _, _, 4, 8],
+                 [_, _, _, _, 5, _, _, _, 1],
+                 [1, 2, _, _, _, 8, _, _, _],
+                 [6, _, 4, 1, _, _, _, _, _],
+                 [_, _, 3, _, 6, 2, _, _, _]],
+        solve(Board),
+	printBoard(Board).
